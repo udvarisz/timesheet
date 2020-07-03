76bc50d970a6e4df10c406efbdbe949bad84dddb
Szudvari <szudvari@gmail.com>
2020-07-03 15:50:15 +0200

    div tidy

--- mysite/time_sheet/admin.py
+++ mysite/time_sheet/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from . import models
 # Register your models here.
 
+class MemberAdmin(admin.ModelAdmin):
+    list_display=['last_name','first_name','pmpsz_number']
 
 class DutyAdmin(admin.ModelAdmin):
     list_display=['date', 'member', 'duty_type', 'plate']
@@ -9,6 +11,6 @@ class DutyAdmin(admin.ModelAdmin):
 class CarAdmin(admin.ModelAdmin):
     list_display=['car_plate', 'car_type', 'owner']
 
-admin.site.register(models.Member)
+admin.site.register(models.Member,MemberAdmin)
 admin.site.register(models.Duty,DutyAdmin)
 admin.site.register(models.Car,CarAdmin)
